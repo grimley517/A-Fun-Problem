dd9f63ff0e854aacb0c71354843d78831d3fa200
grimley517 <ba.jones@me.com>
2014-09-28 23:36:58 +1000

    Automated running of perl script.
    unit tests in runnertests.py

---
+++ runner.py
@@ -0,0 +1,18 @@
+import subprocess
+
+class perlRunner():
+    def __init__ (self,  *args, script = "script.pl"):
+        self.script = script.lower() #to be commercial this would need some input checking.
+        self.args = args
+        self.cmd = ["perl {0}".format(self.script) ,' '.join(self.args)]
+
+    def call(self):
+        ''' Gets the output from the script to be run
+        '''
+        return (subprocess.check_output(self.cmd, shell = True))
+
+    def code(self):
+        '''Gets the return code from the script to be run
+        '''
+        return (subprocess.call(self.cmd, shell = True))
+        
---
+++ runnertest.py
@@ -0,0 +1,30 @@
+import runner as run
+import unittest
+
+class testSequence(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test1(self):
+        #checks that the perlrunner class is running a system command
+        proc = run.perlRunner('-v', script = ' ')
+        self.assertEqual(proc.code(), 0)#test1 fails - basic process failed to run
+
+    def test2(self):
+        # Checks that the perl script can be run with exit code 0
+        proc = run.perlRunner()
+        self.assertEqual(proc.code(), 0)#test2 fails - script.pl may be wrong
+
+    def test3(self):
+        ''' This is not really a test method, just a way of automating a mini-fuzzer.
+
+            this is to inject a number of arguments into the script and see what the outputs are
+            '''
+        testArgs = '1 2 3 4 5 6 7 8 9 10'
+        proc = run.perlRunner(testArgs)
+        print(proc.call())
+        
+
+if __name__ == '__main__':
+    unittest.main()
