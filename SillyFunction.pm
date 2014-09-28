f05f7efaff150e5819a7fe0bcc7811f3d5bec7ee
grimley517 <ba.jones@me.com>
2014-09-28 10:47:48 +1000

    more annotation

--- SillyFunction.pm
+++ SillyFunction.pm
@@ -10,22 +10,29 @@
 # Python or Javascript too.
 #
 package SillyFunction;
+# package is simmilar to the class keyword in python
 
 sub group_products {
+    #sub is simmilar to the def keyword in python
+    #this is a constructor for the class.
   my $products = shift;
   # the $ sign signifies that products is scalar (string, number, reference or file handle
   # shift is used to assign the functions arguments to $products
+  # the my keyword defines the scop as beloning private to the package
   my %brand_type = ();
   # the % sign signifies a hash (dictionary)  essentially an empty dict
   my $grouped_products = [];
-    # again a scalar variable,
+    # again a scalar variable, This is a reference to an empty array, like an empty list in python
   
 
   foreach (@{$products})
   # the @ sign denotes a list (one dimensional array) this is a loop which converts the string held in $products into
-  # a list and then
+    # this would be set by for for product in products: $_ indicates the product variable
+  # a list seperated by spaces the product in products gets put into the special $_ variable
+    
     {
       $brand_type{$_->{brand}} ||= {};
+        #set $brand_type to  some variable called brand ||= makes the default brand a hash reference
       $brand_type{$_->{brand}}->{$_->{type}} = 1;
     }
   foreach (sort keys %brand_type)
@@ -33,10 +40,7 @@ sub group_products {
       my $brand = $_;
       foreach (sort keys %{$brand_type{$brand}}) 
       {
-        push(@{$grouped_products}, 
-          { 
-            brand => $brand, type => $_
-          });
+        push(@{$grouped_products},{ brand => $brand, type => $_});
       }
     }
   $grouped_products;
