98482f25f10a728931b53a3900d14f32e4f3e24c
grimley517 <ba.jones@me.com>
2014-09-29 19:50:27 +1000

    Minor changes

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ class perlRunner():
     def __init__ (self,  *args, script = "script.pl"):
         self.script = script.lower() #to be commercial this would need some input checking.
         self.args = args
-        self.cmd = ["perl {0}".format(self.script) ,' '.join(self.args)]
+        self.cmd = ["perl {0}".format(self.script), ' '.join(self.args)]
 
     def call(self):
         ''' Gets the output from the script to be run
